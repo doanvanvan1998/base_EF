15423a330dc601958939ba7ae3e6a50a04619fa8
The Vu <thevu091193@gmail.com>
2024-02-15 09:37:39 +0700

    add

--- JWTAuthentication.NET6.0/obj/Debug/net6.0/JWTAuthentication.NET6.0.AssemblyInfo.cs
+++ JWTAuthentication.NET6.0/obj/Debug/net6.0/JWTAuthentication.NET6.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAuthentication.NET6.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd72b04af90d779183ff9a28c3c9ec94bd5a397")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAuthentication.NET6.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWTAuthentication.NET6.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
